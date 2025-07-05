6c8099545a6fea168639e835ae42166b330d3fe5
joao.reinertcosta <a@a>
2025-07-05 11:04:15 -0300

    Alterado o appbar e implementado o SingleChildScrollView

--- horizone_app/lib/presentation/pages/getstarted_screen.dart
+++ horizone_app/lib/presentation/pages/getstarted_screen.dart
@@ -13,154 +13,156 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 1,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: Image.asset(
-                    'assets/images/landscape_misurina.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30.0, right: 10.0),
-                    child: DropdownButton<String>(
-                      value: selectedLanguage,
-                      underline: SizedBox(),
-                      selectedItemBuilder: (_) {
-                        return [
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              selectedLanguage,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              selectedLanguage,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              selectedLanguage,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ];
-                      },
-                      icon: Icon(
-                        Icons.arrow_drop_down,
-                        size: 22,
-                        color: Colors.white,
-                      ),
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                      dropdownColor: Color(0xff003566),
-                      items: ['English', 'Português', 'Español'].map((lang) {
-                        return DropdownMenuItem(
-                          value: lang,
-                          child: Text(lang),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState(() {
-                          selectedLanguage = value!;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 250),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 1,
+              child: Stack(
+                children: [
+                  Positioned.fill(
                     child: Image.asset(
-                      'assets/images/logo_horizone.png',
+                      'assets/images/landscape_misurina.png',
                       fit: BoxFit.cover,
                     ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    width: 430,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                      color: Color(0xffF6F1EB),
-                    ),
+                  Align(
+                    alignment: Alignment.topRight,
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: Text(
-                              'Ready to explore beyond boundaries?',
-                              style: TextStyle(
-                                fontSize: 32,
-                                color: Color(0xff003566),
-                                fontWeight: FontWeight.bold,
+                      padding: const EdgeInsets.only(top: 30.0, right: 10.0),
+                      child: DropdownButton<String>(
+                        value: selectedLanguage,
+                        underline: SizedBox(),
+                        selectedItemBuilder: (_) {
+                          return [
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                selectedLanguage,
+                                style: TextStyle(color: Colors.white),
                               ),
-                              textAlign: TextAlign.center,
                             ),
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Center(
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.pushReplacementNamed(
-                                      context,
-                                      '/profileSetUp',
-                                    );
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Color(0xff003566),
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 52,
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'Your Journey Starts Here',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                selectedLanguage,
+                                style: TextStyle(color: Colors.white),
                               ),
-                              Icon(
-                                Icons.airplanemode_on_outlined,
-                                color: Color(0xff003566),
-                                size: 26,
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                selectedLanguage,
+                                style: TextStyle(color: Colors.white),
                               ),
-                            ],
-                          ),
-                        ],
+                            ),
+                          ];
+                        },
+                        icon: Icon(
+                          Icons.arrow_drop_down,
+                          size: 22,
+                          color: Colors.white,
+                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                        dropdownColor: Color(0xff003566),
+                        items: ['English', 'Português', 'Español'].map((lang) {
+                          return DropdownMenuItem(
+                            value: lang,
+                            child: Text(lang),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedLanguage = value!;
+                          });
+                        },
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 250),
+                      child: Image.asset(
+                        'assets/images/logo_horizone.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: 430,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        color: Color(0xffF6F1EB),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              child: Text(
+                                'Ready to explore beyond boundaries?',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  color: Color(0xff003566),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushReplacementNamed(
+                                        context,
+                                        '/profileSetUp',
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Color(0xff003566),
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 52,
+                                      ),
+                                    ),
+                                    child: Text(
+                                      'Your Journey Starts Here',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.airplanemode_on_outlined,
+                                  color: Color(0xff003566),
+                                  size: 26,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- horizone_app/lib/presentation/pages/profilesetup_screen.dart
+++ horizone_app/lib/presentation/pages/profilesetup_screen.dart
@@ -13,101 +13,102 @@ class _ProfileSetUpScreenState extends State<ProfileSetUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffF6F1EB),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 36.0, left: 12.0, right: 12.0),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/getStarted');
-                    },
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Color(0xff003566),
-                      size: 30,
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    'Profile',
-                    style: TextStyle(
-                      color: Color(0xffFF914D),
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF6F1EB),
+        foregroundColor: Color(0xff003566),
+
+        actionsIconTheme: IconThemeData(size: 30),
+        centerTitle: true,
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            color: Color(0xffFF914D),
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
           ),
-          SizedBox(height: 20),
-          SizedBox(
-            child: Padding(
-              padding: const EdgeInsets.all(34.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(fontSize: 16, color: Color(0xffFF914D)),
-                      children: [
-                        TextSpan(text: 'Tell Us '),
-                        TextSpan(
-                          text: 'Who You Are \n',
-                          style: TextStyle(color: Color(0xff003566)),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, size: 30),
+          onPressed: () {
+            Navigator.pushNamed(context, '/getStarted'); // Default back action
+          },
+        ),
+      ),
+
+      backgroundColor: Color(0xffF6F1EB),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 50,
+                  right: 35,
+                  bottom: 35,
+                  left: 35,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color(0xffFF914D),
                         ),
-                        TextSpan(
-                          text: 'And We\'ll Take You Where \nYou Want to Be',
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 18),
-                  OrangeTextForm(nameButton: 'Name'),
-                  SizedBox(height: 18),
-                  OrangeTextBoxForm(
-                    hintText:
-                        'Bio \n \nI am a Flutter developer at Lince Tech, creating fast and elegant mobile apps.',
-                  ),
-                  SizedBox(height: 18),
-                  OrangeTextForm(nameButton: 'Date'),
-                  SizedBox(height: 18),
-                  OrangeTextForm(nameButton: 'Gender'),
-                  SizedBox(height: 18),
-                  OrangeTextForm(nameButton: 'Job Title'),
-                  SizedBox(height: 100,),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context,
-                          '/getStarted'
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff003566),
-                      padding: EdgeInsets.symmetric(horizontal: 52, vertical: 16),
-                    ),
-                    child: Text(
-                      'Continue',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                        children: [
+                          TextSpan(text: 'Tell Us '),
+                          TextSpan(
+                            text: 'Who You Are \n',
+                            style: TextStyle(color: Color(0xff003566)),
+                          ),
+                          TextSpan(
+                            text: 'And We\'ll Take You Where \nYou Want to Be',
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 18),
+                    OrangeTextForm(nameButton: 'Name'),
+                    SizedBox(height: 18),
+                    OrangeTextBoxForm(
+                      nameButton: 'Bio',
+                      hintText:
+                          'I am a Flutter developer at Lince Tech, creating fast and elegant mobile apps.',
+                    ),
+                    SizedBox(height: 18),
+                    OrangeTextForm(nameButton: 'Date'),
+                    SizedBox(height: 18),
+                    OrangeTextForm(nameButton: 'Gender'),
+                    SizedBox(height: 18),
+                    OrangeTextForm(nameButton: 'Job Title'),
+                    SizedBox(height: 100),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/getStarted');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff003566),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 52,
+                          vertical: 16,
+                        ),
+                      ),
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -138,8 +139,13 @@ class OrangeTextForm extends StatelessWidget {
 }
 
 class OrangeTextBoxForm extends StatelessWidget {
-  OrangeTextBoxForm({required this.hintText, super.key});
+  OrangeTextBoxForm({
+    required this.hintText,
+    required this.nameButton,
+    super.key,
+  });
 
+  String nameButton = 'Click me';
   String hintText = 'Hint Text';
 
   @override
@@ -151,8 +157,9 @@ class OrangeTextBoxForm extends StatelessWidget {
         expands: true, // and this
         keyboardType: TextInputType.multiline,
         decoration: InputDecoration(
+          labelText: nameButton,
           hintText: hintText,
-          labelStyle: TextStyle(color: Color(0xff020101)),
+          labelStyle: TextStyle(color: Color(0xff020101), ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
             borderSide: BorderSide(color: Color(0xffFF914D)),
