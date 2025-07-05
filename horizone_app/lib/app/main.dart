c5500b4b7177c065867a9971f1dc23f27ff1cc5b
joao.reinertcosta <a@a>
2025-07-05 13:52:31 -0300

    Implementado internazionalização l10n

--- horizone_app/lib/app/main.dart
+++ horizone_app/lib/app/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:horizone_app/app/routes.dart';
 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(
-      MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: MyApp()
-      ),
-  );
+  runApp(MyApp());
 }
--- horizone_app/lib/app/routes.dart
+++ horizone_app/lib/app/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:horizone_app/presentation/pages/getstarted_screen.dart';
 import 'package:horizone_app/presentation/pages/profilesetup_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import '../generated/l10n.dart';
 import '../presentation/pages/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,6 +13,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      locale: const Locale('pt'),
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+        Locale('pt'), // Portuguese
+      ],
       title: 'Horizone',
       initialRoute: '/',
       routes: {
