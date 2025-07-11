2f36b8d4aa16f8a66df25791d9ec8115daad3772
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-11 02:08:48 -0300

    feat: add SharedPreferences for local data storage

--- horizone_app/lib/presentation/state/locale_provider.dart
+++ horizone_app/lib/presentation/state/locale_provider.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleProvider extends ChangeNotifier {
   Locale _locale;
 
-  LocaleProvider(this._locale);
-
   Locale? get locale => _locale;
+
+  LocaleProvider(this._locale) {
+    _loadLocale();
+  }
 
   void setLocale(Locale newLocale) {
     if (_locale == newLocale) return;
     _locale = newLocale;
+    _saveLocale(newLocale);
     notifyListeners();
+  }
+
+  Future<void> _loadLocale() async {
+    final perfs = await SharedPreferences.getInstance();
+    final languageCode = perfs.getString('languageCode') ?? 'pt';
+    _locale = Locale(languageCode);
+    notifyListeners();
+  }
+
+  Future<void> _saveLocale(Locale locale) async {
+    final perfs = await SharedPreferences.getInstance();
+    await perfs.setString('languageCode', locale.languageCode);
   }
 }
--- horizone_app/lib/presentation/state/theme_provider.dart
+++ horizone_app/lib/presentation/state/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
@@ -7,8 +8,25 @@ class ThemeProvider extends ChangeNotifier {
 
   bool get isDarkMode => _themeMode == ThemeMode.dark;
 
+  ThemeProvider() {
+    _loadTheme();
+  }
+
   void toggleTheme(bool isOn) {
     _themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    _saveTheme(_themeMode);
     notifyListeners();
+  }
+
+  Future<void> _loadTheme() async {
+    final perfs = await SharedPreferences.getInstance();
+    final isDarkMode = perfs.getBool('isDarkMode') ?? false;
+    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  Future<void> _saveTheme(ThemeMode mode) async {
+    final perfs = await SharedPreferences.getInstance();
+    await perfs.setBool('isDarkMode', mode == ThemeMode.dark);
   }
 }
