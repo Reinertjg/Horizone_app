8f0425c8b8ed7d26f6b830b07cf5db20db79d052
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-15 05:09:29 -0300

    Refactor app routes into AppRoutes class and move MyApp widget to main.dart; update MaterialApp to use AppRoutes.routes and manage theme and locale with providers in main.dart

--- horizone_app/lib/app/main.dart
+++ horizone_app/lib/app/main.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:horizone_app/app/routes.dart';
 import 'package:provider/provider.dart';
 
+import '../generated/l10n.dart';
+import '../presentation/app_theme.dart';
 import '../presentation/state/locale_provider.dart';
 import '../presentation/state/theme_provider.dart';
 
-
 void main() {
-        runApp(
-          MultiProvider(
-            providers: [
-              ChangeNotifierProvider(
-                create: (_) => LocaleProvider(const Locale('pt')),
-              ),
-              ChangeNotifierProvider(
-                create: (_) => ThemeProvider(),
-              ),
-            ],
-            child: const MyApp(),
-          ),
-        );
-      }
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LocaleProvider(const Locale('pt')),
+        ),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final localeProvider = Provider.of<LocaleProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return MaterialApp(
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      themeMode: themeProvider.themeMode,
+      debugShowCheckedModeBanner: false,
+      locale: localeProvider.locale,
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+        Locale('pt'), // Portuguese
+      ],
+      title: 'Horizone',
+      initialRoute: '/',
+      routes: AppRoutes.routes,
+    );
+  }
+}
--- horizone_app/lib/app/routes.dart
+++ horizone_app/lib/app/routes.dart
@@ -1,48 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:horizone_app/presentation/pages/getstarted_screen.dart';
-import 'package:horizone_app/presentation/pages/profilesetup_screen.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
 
-import '../generated/l10n.dart';
-import '../presentation/app_theme.dart';
 import '../presentation/pages/dashboard_screen.dart';
+import '../presentation/pages/getstarted_screen.dart';
+import '../presentation/pages/profilesetup_screen.dart';
 import '../presentation/pages/splash_screen.dart';
-import '../presentation/state/locale_provider.dart';
-import '../presentation/state/theme_provider.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final localeProvider = Provider.of<LocaleProvider>(context);
-    final themeProvider = Provider.of<ThemeProvider>(context);
-    return MaterialApp(
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
-      themeMode: themeProvider.themeMode,
-      debugShowCheckedModeBanner: false,
-      locale: localeProvider.locale,
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en'), // English
-        Locale('es'), // Spanish
-        Locale('pt'), // Portuguese
-      ],
-      title: 'Horizone',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/getStarted': (context) => const GetStartedScreen(),
-        '/profileSetUp': (context) => const ProfileSetUpScreen(),
-        '/dashboard': (context) => const DashboardScreen(),
-      },
-    );
-  }
+class AppRoutes {
+  static final Map<String, WidgetBuilder> routes = {
+    '/': (context) => const SplashScreen(),
+    '/getStarted': (context) => const GetStartedScreen(),
+    '/profileSetup': (context) => const ProfileSetUpScreen(),
+    '/dashboard': (context) => const DashboardScreen(),
+  };
 }
