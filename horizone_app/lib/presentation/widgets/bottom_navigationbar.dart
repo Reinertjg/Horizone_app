9a07099a77bda0d75d6e43066f27fa334becb7de
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-19 18:28:03 -0300

    Refactor BottomNavigationBar and add navigation logic.

--- horizone_app/lib/presentation/widgets/bottom_navigationbar.dart
+++ horizone_app/lib/presentation/widgets/bottom_navigationbar.dart
@@ -2,52 +2,60 @@ import 'package:flutter/material.dart';
 
 import '../../generated/l10n.dart';
 
- BottomNavigationBar bottomNavigationBar (BuildContext context) {
-   return BottomNavigationBar(
-     backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-     unselectedItemColor: Theme.of(context).hintColor,
-     selectedItemColor: Theme.of(context).hintColor,
-     items: [
-       BottomNavigationBarItem(
-         icon: Icon(Icons.home, color: Theme.of(context).primaryColor),
-         label: S.of(context).home,
-         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-       ),
-       BottomNavigationBarItem(
-         icon: Icon(
-           Icons.travel_explore,
-           color: Theme.of(context).primaryColor,
-         ),
-         label: 'Search',
-       ),
-       BottomNavigationBarItem(
-         icon: Icon(
-           Icons.calendar_month,
-           color: Theme.of(context).primaryColor,
-         ),
-         label: 'Notifications',
-       ),
-       BottomNavigationBarItem(
-         icon: Icon(
-           Icons.person_pin_rounded,
-           color: Theme.of(context).primaryColor,
-         ),
-         label: 'Profile',
-       ),
-     ],
-     currentIndex: 0,
-     // Set the current index to the first item
-     onTap: (index) {
-       switch (index) {
-         case 0:
-           break;
-         case 1:
-           break;
-         case 2:
-           break;
-         case 3:
-           break;
-       }
-     },
-   );
- }
+BottomNavigationBar bottomNavigationBar(
+  BuildContext context,
+  int currentIndex,
+) {
+  return BottomNavigationBar(
+    backgroundColor: Theme.of(context).primaryColor,
+    unselectedItemColor: Theme.of(context).hintColor,
+    selectedItemColor: Theme.of(context).hintColor,
+    elevation: 0,
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home, color: Theme.of(context).primaryColor),
+        label: S.of(context).home,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.mode_of_travel_outlined,
+          color: Theme.of(context).primaryColor,
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        label: 'Search',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.calendar_month, color: Theme.of(context).primaryColor),
+        label: 'Notifications',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.person_pin_rounded,
+          color: Theme.of(context).primaryColor,
+        ),
+        label: 'Profile',
+      ),
+    ],
+    currentIndex: currentIndex,
+    // Set the current index to the first item
+    onTap: (index) {
+      switch (index) {
+        case 0:
+          if (currentIndex != 0) {
+            Navigator.pushNamed(context, '/dashboard');
+          }
+          break;
+        case 1:
+          if (currentIndex != 1) {
+            Navigator.pushNamed(context, '/interview');
+          }
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    },
+  );
+}
