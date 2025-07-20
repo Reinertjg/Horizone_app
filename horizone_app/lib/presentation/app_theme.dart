aec7f412464abe4192cb7a6ce058c17d0a8966c7
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-20 08:34:46 -0300

    Refactor: Update app theme colors for light and dark modes.

--- horizone_app/lib/presentation/app_theme.dart
+++ horizone_app/lib/presentation/app_theme.dart
@@ -1,20 +1,19 @@
-
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme() {
   return ThemeData(
-      scaffoldBackgroundColor: Color(0xFFF6F1EB),
-      primaryColor: Color(0xFF003566),
-      hintColor: Color(0xFFFF914D),
-      highlightColor: Color(0xFFF6F1EB)
+    scaffoldBackgroundColor: Color(0xFFF4F8FB),
+    primaryColor: Color(0xFF005BB1), // Azul vívido, moderno
+    hintColor: Color(0xFFFF9900), // Cinza claro, neutro
+    highlightColor: Color(0xFFFFFFFF), // Azul suave para interações
   );
 }
 
 ThemeData darkTheme() {
   return ThemeData(
-    scaffoldBackgroundColor: Color(0xFF1E1E1E),
-    primaryColor: Color(0xFFB9D6F2),
-    hintColor: Color(0xFFFBAE7A),
-    highlightColor: Color(0xFF1E1E1E),
+    scaffoldBackgroundColor: Color(0xFF1C1C1E),
+    primaryColor: Color(0xFF90CAF9),
+    hintColor: Color(0xFFFFB74D),
+    highlightColor: Color(0xFF2C2C2E),
   );
 }
