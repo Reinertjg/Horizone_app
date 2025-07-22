98d2e2e7f6090b50600c1bf7a68a5b65d3aaa613
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-22 19:35:40 -0300

    Refactor ContinueButton to use pushNamed for navigation and add mounted check.

--- horizone_app/lib/presentation/widgets/continue_button.dart
+++ horizone_app/lib/presentation/widgets/continue_button.dart
@@ -1,55 +1,54 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-  import '../../data/repositories/profile_repository_impl.dart';
+import '../../data/repositories/profile_repository_impl.dart';
 import '../../database/daos/profile_dao.dart';
-import '../../domain/repositories/profile_repository.dart';
 import '../../domain/usecases/profile_usecase.dart';
 import '../state/profileform_provider.dart';
 
-  class ContinueButton extends StatelessWidget {
-    ContinueButton({
-      required this.title,
-      required this.pathRoute,
-      required this.formKey,
-      super.key,
-    });
+class ContinueButton extends StatelessWidget {
+  const ContinueButton({
+    required this.title,
+    required this.pathRoute,
+    required this.formKey,
+    super.key,
+  });
 
-    final String title;
-    final String pathRoute;
-    final GlobalKey<FormState> formKey;
+  final String title;
+  final String pathRoute;
+  final GlobalKey<FormState> formKey;
 
-    @override
-    Widget build(BuildContext context) {
-      final formProvider = Provider.of<ProfileFormProvider>(context);
+  @override
+  Widget build(BuildContext context) {
+    final formProvider = Provider.of<ProfileFormProvider>(context);
 
+    return ElevatedButton(
+      onPressed: () async {
+        if (formKey.currentState!.validate()) {
+          final profile = formProvider.toEntity();
 
-      return ElevatedButton(
-        onPressed: () async {
-          if (formKey.currentState!.validate()) {
-            final profile = formProvider.toEntity();
+          final dao = ProfileDao();
+          final repository = ProfileRepositoryImpl(dao);
+          final useCase = ProfileUseCase(repository);
 
-            final dao = ProfileDao();
-            final repository = ProfileRepositoryImpl(dao);
-            final useCase = ProfileUseCase(repository);
+          await useCase.insert(profile);
 
-            await useCase.insert(profile);
-
-            Navigator.pushReplacementNamed(context, pathRoute);
+          if (context.mounted) {
+            Navigator.pushNamed(context, pathRoute);
           }
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Theme.of(context).primaryColor,
-          padding: const EdgeInsets.symmetric(horizontal: 52, vertical: 12),
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Theme.of(context).primaryColor,
+        padding: const EdgeInsets.symmetric(horizontal: 52, vertical: 12),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 16,
+          color: Theme.of(context).highlightColor,
+          fontWeight: FontWeight.bold,
         ),
-        child: Text(
-          title,
-          style: TextStyle(
-            fontSize: 16,
-            color: Theme.of(context).highlightColor,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      );
-    }
+      ),
+    );
   }
+}
