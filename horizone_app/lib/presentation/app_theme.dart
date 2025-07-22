84c6c2ec72023f0d7c2e6223a2205aaff84b9a5c
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-22 19:36:40 -0300

    Refactor: update app theme colors.

--- horizone_app/lib/presentation/app_theme.dart
+++ horizone_app/lib/presentation/app_theme.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 
 ThemeData lightTheme() {
   return ThemeData(
-    scaffoldBackgroundColor: Color(0xFFF4F8FB),
-    primaryColor: Color(0xFF005BB1), // Azul vívido, moderno
-    hintColor: Color(0xFFFF8800), // Cinza claro, neutro
-    highlightColor: Color(0xFFFFFFFF), // Azul suave para interações
+    scaffoldBackgroundColor: Color(0xFFF6F1EB),
+    primaryColor: Color(0xFF005BB1),
+    hintColor: Color(0xFFFF981F),
+    highlightColor: Color(0xFFFFFFFF),
+    focusColor: Color(0xFFFFD29F), // Verde para foco
+    disabledColor: Color(0xFFBDBDBD), // Cinza para desabilitado
+    shadowColor: Color(0xFF000000),
   );
 }
 
@@ -15,5 +18,8 @@ ThemeData darkTheme() {
     primaryColor: Color(0xFF90CAF9),
     hintColor: Color(0xFFFFB74D),
     highlightColor: Color(0xFF2C2C2E),
+    focusColor: Color(0xFFFF8800), // Verde claro para foco
+    disabledColor: Color(0xFF616161), // Cinza escuro para desabilitado
+    shadowColor: Color(0xFFFFFFFF),
   );
 }
