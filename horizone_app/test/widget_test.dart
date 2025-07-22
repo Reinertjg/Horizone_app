8a83b54a58cf0b3ac24c1eb1121c6090c78dd842
Reinertjg <jgcosta.rei4@gmail.com>
2025-07-22 19:37:52 -0300

    feat: integrate Google Fonts. Adds the `google_fonts` package to the project dependencies.

--- horizone_app/test/widget_test.dart
+++ horizone_app/test/widget_test.dart
@@ -7,9 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:horizone_app/app/main.dart';
-import 'package:horizone_app/app/routes.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
