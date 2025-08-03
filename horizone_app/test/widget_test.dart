7c0d301b5db92860e5e7022860cb39dbbcb8c4b1
Reinertjg <jgcosta.rei4@gmail.com>
2025-08-03 17:36:13 -0300

    feat: add image_picker dependency.

--- horizone_app/test/widget_test.dart
+++ horizone_app/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:horizone_app/app/main.dart';
+import 'package:horizone_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (tester) async {
