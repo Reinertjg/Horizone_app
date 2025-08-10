f56c9a5e54be2040de59e251974ac652cc9d1916
Reinertjg <jgcosta.rei4@gmail.com>
2025-08-10 17:45:02 -0300

    Refactor DashboardScreen UI for improved layout and user profile display

--- lib/presentation/pages/dashboard_screen.dart
+++ lib/presentation/pages/dashboard_screen.dart
@@ -8,7 +8,6 @@ import '../../generated/l10n.dart';
 import '../theme_color/app_colors.dart';
 import '../widgets/iconbutton_notifications.dart';
 import '../widgets/iconbutton_settings.dart';
-import '../widgets/profile_widgets/bottom_navigationbar.dart';
 
 /// The main screen displayed after user login,
 /// showing a personalized welcome and main navigation options.
@@ -47,48 +46,70 @@ class _DashboardScreenState extends State<DashboardScreen> {
     final colors = Theme.of(context).extension<AppColors>()!;
     return Scaffold(
       backgroundColor: colors.primary,
-      appBar: AppBar(
-        backgroundColor: colors.primary,
-        elevation: 0,
-        toolbarHeight: 100,
-        title: Text(
-          '${S.of(context).welcome}, '
-              '${profiles.isNotEmpty ? profiles[0].name : 'User'}',
-          style: GoogleFonts.nunito(color: colors.secondary),
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.only(top: 28, left: 10.0, bottom: 25.0),
-          child: Card(
-            color: colors.secondary,
-            shape: const CircleBorder(),
-            child: ClipOval(
-              child: Image.network(
-                'https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Row(
-              children: [
-                IconbuttonSettings(),
-                const SizedBox(width: 10),
-                IconbuttonNotifications(),
-              ],
-            ),
-          ),
-        ],
-      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(22.0),
+            padding: const EdgeInsets.all(12.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [],
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 380,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: colors.secondary.withValues(
+                                    alpha: 0.1,
+                                  ),
+                                  width: 2,
+                                ),
+                                color: colors.secondary,
+                              ),
+                              child: ClipOval(
+                                child: Image.asset(
+                                  'assets/images/user_default_photo.png',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            Expanded(
+                              child: Text(
+                                '${S.of(context).welcome}, '
+                                '${profiles.isNotEmpty ? profiles[0].name : 'User'}',
+                                style: GoogleFonts.nunito(
+                                  color: colors.secondary,
+                                  fontSize: 22,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            Row(
+                              children: [
+                                IconbuttonSettings(),
+                                const SizedBox(width: 6),
+                                IconbuttonNotifications(),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
