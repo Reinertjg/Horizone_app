efbe91a66083b1d94cf7eb3d25397ab579f2fdfa
Reinertjg <jgcosta.rei4@gmail.com>
2025-08-18 13:14:39 -0300

    Feat: Add Google Maps API key and configure platform permissions.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAgT9pV0ONamMF8ByF008OT7lf4-1oAFd0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
