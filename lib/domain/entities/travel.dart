da85817d540ed5d8b4efec7cef927f91320363aa
Reinertjg <jgcosta.rei4@gmail.com>
2025-08-31 11:29:55 -0300

    Refactor Travel entity and related classes

--- lib/domain/entities/travel.dart
+++ lib/domain/entities/travel.dart
@@ -2,8 +2,7 @@ import 'travelstop.dart';
 
 /// A class representing a trip.
 class Travel {
-  /// The unique identifier of the trip.
-  final int id;
+
   /// The title or name of the trip.
   final String title;
   /// The start date of the trip.
@@ -17,52 +16,55 @@ class Travel {
   /// The type of experience the trip offers.
   final String experienceType;
   /// The Origin Place the Trip
-  final PlacePoint? originPlace;
+  final String originPlace;
   final String originLabel;
   /// The Destination Place the Trip
-  final PlacePoint? destinationPlace;
+  final String destinationPlace;
   final String destinationLabel;
 
   /// Constructs a new [Travel] object.
   Travel({
-    required this.id,
     required this.title,
     required this.startDate,
     required this.endDate,
     required this.meansOfTransportation,
     required this.numberOfParticipants,
     required this.experienceType,
-    this.originPlace,
+    required this.originPlace,
     required this.originLabel,
-    this.destinationPlace,
+    required this.destinationPlace,
     required this.destinationLabel
   });
 
   /// Converts the [Travel] object to a map.
-  Map<String, dynamic> toMap() => {
-    'title': title,
-    'startDate': startDate,
-    'endDate': endDate,
-    'meansOfTransportation': meansOfTransportation,
-    'numberOfParticipants': numberOfParticipants,
-    'experienceType': experienceType,
-    'originLabel': originLabel,
-    'destinationLabel': destinationLabel,
-    'originPlace': originPlace,
-    'destinationPlace': destinationPlace
-  };
+  Map<String, dynamic> toMap() {
+    final map = {
+      'title': title,
+      'startDate': startDate,
+      'endDate': endDate,
+      'meansOfTransportation': meansOfTransportation,
+      'numberOfParticipants': numberOfParticipants,
+      'experienceType': experienceType,
+      'originLabel': originLabel,
+      'destinationLabel': destinationLabel,
+      'originPlace': originPlace,
+      'destinationPlace': destinationPlace
+    };
+    return map;
+  }
 
   /// Creates a [Travel] object from a map.
   static Travel fromMap(Map<String, dynamic> map) {
     return Travel(
-      id: map['id'],
       title: map['title'],
       startDate: map['startDate'],
       endDate: map['endDate'],
       meansOfTransportation: map['meansOfTransportation'],
       numberOfParticipants: map['numberOfParticipants'],
       experienceType: map['experienceType'],
+      originPlace: map['originPlace'],
       originLabel: map['originLabel'],
+      destinationPlace: map['destinationPlace'],
       destinationLabel: map['destinationLabel']
     );
   }
