b45523ded8ab8f2347c9af3ee323ba1e37e7af25
Reinertjg <jgcosta.rei4@gmail.com>
2025-09-04 21:21:14 -0300

    Docs: Add dartdocs to PlacesService and its methods

--- lib/domain/usecases/get_place_suggestions.dart
+++ lib/domain/usecases/get_place_suggestions.dart
@@ -2,13 +2,18 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
 
+/// Represents a service for fetching place suggestions
+/// from the Google Places API.
 class PlacesService {
+  /// The API key for the Google Places API.
   final String apiKey;
   final _uuid = const Uuid();
   String _sessionToken = '';
 
+  /// Creates a new [PlacesService] object.
   PlacesService({required this.apiKey});
 
+  /// Fetches place suggestions based on the given input.
   Future<List<Map<String, String>>> fetchSuggestions(String input) async {
     if (input.trim().length < 2) return [];
 
@@ -21,12 +26,10 @@ class PlacesService {
         'Content-Type': 'application/json; charset=utf-8',
         'X-Goog-Api-Key': apiKey,
         'X-Goog-FieldMask':
-        'suggestions.placePrediction.placeId,suggestions.placePrediction.text',
+            'suggestions.placePrediction.placeId,suggestions.placePrediction.text',
         'X-Goog-Maps-Platform-Session-Token': _sessionToken,
       },
-      body: jsonEncode({
-        'input': input,
-      }),
+      body: jsonEncode({'input': input}),
     );
 
     if (resp.statusCode != 200) return [];
@@ -35,15 +38,18 @@ class PlacesService {
     final list = (data['suggestions'] as List? ?? [])
         .map((e) => e['placePrediction'])
         .where((p) => p != null)
-        .map((p) => {
-      'placeId': p['placeId'] as String,
-      'description': (p['text']?['text'] as String?) ?? '',
-    })
+        .map(
+          (p) => {
+            'placeId': p['placeId'] as String,
+            'description': (p['text']?['text'] as String?) ?? '',
+          },
+        )
         .toList();
 
     return list;
   }
 
+  /// Resets the session token for the next request.
   void resetSession() {
     _sessionToken = '';
   }
