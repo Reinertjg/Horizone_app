75d3e548041943c7c3ae056c34e46f704d7dd39b
Reinertjg <jgcosta.rei4@gmail.com>
2025-09-10 02:31:35 -0300

    Refactor: Move ConfirmDeleteDialog to delete_account_tile.dart

--- lib/presentation/widgets/settings_widgets/delete_accoun_tile.dart
+++ lib/presentation/widgets/settings_widgets/delete_accoun_tile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import '../../../domain/usecases/profile_usecase.dart';
 import '../../../generated/l10n.dart';
-import 'confirm_delete_dialog.dart';
+import '../../../repositories/profile_repository_impl.dart';
+import '../../pages/getstarted_screen.dart';
+import '../../theme_color/app_colors.dart';
 
 /// A widget that displays a red "Delete Account" text button.
 ///
@@ -17,7 +20,7 @@ class DeleteAccountTile extends StatelessWidget {
         showDialog(
             context: context,
             builder: (context) =>
-            ConfirmDeleteDialog()
+            _ConfirmDeleteDialog()
         );
       },
       child: Text(
@@ -26,6 +29,49 @@ class DeleteAccountTile extends StatelessWidget {
           color: Color(0xFFA50101),
         ),
       ),
+    );
+  }
+}
+
+/// A confirmation dialog
+/// That asks the user if they really want to delete their profile.
+class _ConfirmDeleteDialog extends StatelessWidget {
+  /// Creates a custom [_ConfirmDeleteDialog].
+  const _ConfirmDeleteDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).extension<AppColors>()!;
+    return AlertDialog(
+      backgroundColor: colors.primary,
+      title: Text(S.of(context).deleteAccountAsk),
+      content: Text(S.of(context).deleteMessage),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'Cancel'),
+          child: Text(
+            S.of(context).cancel,
+            style: TextStyle(color: colors.secondary),
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final repository = ProfileRepositoryImpl();
+            final useCase = ProfileUseCase(repository);
+
+            await useCase.delete();
+            if (!context.mounted) return;
+            await Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => GetStartedScreen()),
+                  (route) => false,
+            );
+          },
+          child: Text(
+            S.of(context).delete,
+            style: TextStyle(color: colors.secondary),
+          ),
+        ),
+      ],
     );
   }
 }
