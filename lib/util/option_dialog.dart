3ccf5899b3fa5eaa455b6cd31c1dbdd06d7b9043
joao.reinertcosta <jgcosta.rei4@gmail.com>
2025-09-10 16:52:39 -0300

    Refactor: Enhance OptionDialog with animation and InterviewFab

--- lib/util/option_dialog.dart
+++ lib/util/option_dialog.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../generated/l10n.dart';
 import '../presentation/theme_color/app_colors.dart';
+import '../presentation/widgets/interview_widgets/interview_fab.dart';
 
 /// Dialog for selecting an option.
-class OptionDialog extends StatelessWidget {
+class OptionDialog extends StatefulWidget {
   final String title;
   final String message;
   final String buttonText;
 
-  /// Creates an [OptionDialog] widget.
   const OptionDialog({
     super.key,
     required this.title,
@@ -18,58 +19,78 @@ class OptionDialog extends StatelessWidget {
   });
 
   @override
+  State<OptionDialog> createState() => _OptionDialogState();
+}
+
+class _OptionDialogState extends State<OptionDialog>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    )..forward();
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+  }
+
+  @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).extension<AppColors>()!;
+    return ScaleTransition(
+      scale: _animation,
+      child: AlertDialog(
+        backgroundColor: colors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        title: Text(
+          widget.title,
+          style: GoogleFonts.raleway(color: colors.quaternary),
+        ),
+        titlePadding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
+        contentPadding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
+        content: Text(
+          widget.message,
+          style: GoogleFonts.raleway(color: colors.quaternary),
+        ),
 
-    return AlertDialog(
-      backgroundColor: colors.primary,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      title: Text(title, style: TextStyle(color: colors.quaternary)),
-      titlePadding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0.0),
-      contentPadding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0.0),
-      content: Text(message,  style: TextStyle(color: colors.quaternary)),
-      actions: [
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colors.secondary,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop(false); // Typically cancel returns false
-                },
-                child: Text(
-                  S.of(context).cancel,
-                  style: TextStyle(color: colors.quaternary),
+        actions: [
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: InterviewFab(
+                  nameButton: S.of(context).cancel,
+                  onPressed: () async {
+                    Navigator.of(context).pop(false);
+                  },
                 ),
               ),
-            ),
-            const SizedBox(width: 8), // Adds space between buttons
-            Expanded(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colors.secondary,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: Text(
-                  buttonText,
-                  style: TextStyle(color: colors.quaternary),
+              const SizedBox(width: 8),
+              Expanded(
+                child: InterviewFab(
+                  nameButton: widget.buttonText,
+                  onPressed: () async {
+                    Navigator.of(context).pop(true);
+                  },
                 ),
               ),
-            ),
-          ],
-        )
-      ],
-      actionsPadding: const EdgeInsets.all(16.0),
+            ],
+          ),
+        ],
+        actionsPadding: const EdgeInsets.all(16),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
